e137ab44c312e27a7ed802db0384943b5935d40e
fabio0507 <fabio@students.untidar.ac.id>
2026-08-26 15:26:31 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,20 @@
+# Progam Menampilkan Data Diri
+
+print("=" * 49)
+print("DATA DIRI MAHASISWA".center(49))
+print("=" * 49)
+
+# Menampilkan Data Diri
+print("\nNama\t\t : Veganza Fabio Prasetya")
+print("NIM\t\t\t : 2605060006")
+print("TTL\t\t\t : Kota Magelang, 12 Mei 2007")
+print("Jenis Kelamin: Laki-laki")
+print("Hobi\t\t : Futsal")
+
+# Menampilkan Garis Penutup
+print("\n" + "=" * 49)
+print("TERIMA KASIH".center(49))
+print("=" * 49)
+
+
+
